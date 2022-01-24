be426ad2857b66e8cc65aa0306f45bfb49da08e7
OliviaYJH <0703olivia@naver.com>
2022-01-24 21:28:33 +0900

    [FEAT] enemy 랜덤하게 옷 입히기

---
+++ DeliveryRun/Assets/Scripts/RandomClothing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomClothing : MonoBehaviour
+{
+    private int random_number;
+    void Start()
+    {
+        random_number = Random.Range(0,12);
+        gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetChild(random_number).gameObject.SetActive(true);
+    }
+}
