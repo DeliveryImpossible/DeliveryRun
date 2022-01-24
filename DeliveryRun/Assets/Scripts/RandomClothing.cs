4b6b6467d0ac797e2c9f263c4666bdcb3b6796d3
OliviaYJH <0703olivia@naver.com>
2022-01-24 21:30:53 +0900

    [FIX] random range number

--- DeliveryRun/Assets/Scripts/RandomClothing.cs
+++ DeliveryRun/Assets/Scripts/RandomClothing.cs
@@ -7,7 +7,7 @@ public class RandomClothing : MonoBehaviour
     private int random_number;
     void Start()
     {
-        random_number = Random.Range(0,12);
+        random_number = Random.Range(0,11);
         gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetChild(random_number).gameObject.SetActive(true);
     }
 }
