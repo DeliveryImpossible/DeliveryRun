1665593e452b287310e55c0b78dfb618382f2986
YEJIN <yejon9858@gmail.com>
2022-01-30 19:10:40 +0900

    [FEAT] Player Respwan

---
+++ DeliveryRun/Assets/Scripts/Player/PlayerRespwan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespwan : MonoBehaviour
+{
+    public Vector3 playerRespwanLocation;
+
+    private void Update()
+    {
+        if(transform.position.y < -30)
+        {
+            transform.position = playerRespwanLocation;
+        }
+    }
+}
