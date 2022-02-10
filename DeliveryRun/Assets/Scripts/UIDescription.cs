c78485dacc3fc45cc7888fc3c17f15276427fea3
chaeyeong <shimcy2010@gmail.com>
2022-02-10 15:48:04 +0900

    [UI] #33 - 조작방법 UI

---
+++ DeliveryRun/Assets/Scripts/UIDescription.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDescription : MonoBehaviour
+{
+    public GameObject firstPanelfirstText;
+    public GameObject firstPanelsecondText;
+    public GameObject firstPanelfirstButton;
+    public GameObject firstPanelsecondButton;
+
+    public GameObject secondPanelfirstText;
+    public GameObject secondPanelsecondText;
+    public GameObject secondPanelfirstButton;
+    public GameObject secondPanelsecondButton;
+
+    public void navigateToSecondAtFirstPanel()
+    {
+        firstPanelfirstButton.SetActive(false);  
+        firstPanelfirstText.SetActive(false);
+        firstPanelsecondButton.SetActive(true);
+        firstPanelsecondText.SetActive(true);
+    }
+
+    public void navigateToSecondAtSecondPanel()
+    {
+        secondPanelfirstButton.SetActive(false);
+        secondPanelfirstText.SetActive(false);
+        secondPanelsecondButton.SetActive(true);
+        secondPanelsecondText.SetActive(true);
+    }
+}
